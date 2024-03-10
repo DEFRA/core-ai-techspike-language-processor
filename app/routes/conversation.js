aeb89bbf6862a8f1bd5494929c1f697f03855cec
steve-dickinson <steve.dickinson@defra.gov.uk>
2024-03-10 19:10:14 +0000

    Update to allow querystring

--- app/routes/conversation.js
+++ app/routes/conversation.js
@@ -1,13 +1,26 @@
-const { askQuestionWithSources } = require('../conversation')
+const { askQuestionWithSources, askQuestion } = require('../conversation')
 
-module.exports = {
+module.exports = [{
   method: 'GET',
   path: '/conversation',
   options: {
     tags: ['api'],
     handler: async (request, h) => {
-      const results = await askQuestionWithSources('What are we doing keep britain tidy?')
+      const question = request.query.question
+      const results = await askQuestion(question)
       return h.response(results).code(200)
     }
   }
-}
+},
+{
+  method: 'GET',
+  path: '/conversation-with-source',
+  options: {
+    tags: ['api'],
+    handler: async (request, h) => {
+      const question = request.query.question
+      const results = await askQuestionWithSources(question)
+      return h.response(results).code(200)
+    }
+  }
+}]
